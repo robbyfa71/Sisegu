d3650b109c528067c33ce72bbd167acb964ee71c
robbyfa71 <robbyfirdauzy@gmail.com>
2025-05-04 14:51:22 +0700

    update struktur model

--- apps/administration/apps.py
+++ apps/administration/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MembersConfig(AppConfig):
+class AdministrationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.members'
+    name = 'apps.administration'
